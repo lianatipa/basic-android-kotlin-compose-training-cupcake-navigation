0ba4649e249462d591480b2ec8b1f7378b5d1bcc
lianatipa <144299070+lianatipa@users.noreply.github.com>
2023-09-24 21:22:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/data/DataSource.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake.data
+
+import com.example.cupcake.R
+object DataSource {
+    //disini saya menambahkan beberapa opsi
+    val flavors = listOf(
+        R.string.chocolate,
+        R.string.red_velvet,
+        R.string.salted_caramel,
+        R.string.choose_flavor,
+        R.string.flavor,
+        R.string.coffee,
+
+
+    )
+
+
+    val quantityOptions = listOf(
+        Pair(R.string.One_cupcake, 1),
+        Pair(R.string.Six_cupcakes, 6),
+        Pair(R.string.Twelve_Cupcakes, 12)
+
+    )
+}
+
